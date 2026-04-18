02118d842ef3f16962367c98634333662c97f470
azureblueday <gamerspace32@gmail.com>
2026-04-17 23:58:19 -0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,4 +1,3 @@
-local GameID = game.GameId
 local Exploit = identifyexecutor()
 local Player = game:GetService("Players").LocalPlayer
 
@@ -8,10 +7,4 @@ if table.find(poop, Exploit) then
     return Player:Kick("Nova | Executor is not supported by Nova.")
 end
 
-if game.GameId == 8558141897 then -- flag
-    Player:Kick("Nova | Flag Football is temporarily disabled due to technical errors.")
-end
-
-if game.GameId ~= 8558141897 then
-    loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/2259faaddfcaa0d5acb74e84b2cd96b5.lua"))()
-end
+loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/2259faaddfcaa0d5acb74e84b2cd96b5.lua"))()
