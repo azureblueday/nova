7c4d5a3734980ae880779e31e77d007f9ebcaf88
azureblueday <gamerspace32@gmail.com>
2026-04-18 18:05:17 -0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -7,7 +7,7 @@ if table.find(poop, Exploit) then
     return Player:Kick("Nova | Executor is not supported by Nova.")
 end
 
-warn("thank you so much for using the script! - nova (@xm5l)")
+warn("thank you so much for using the script! - nova")
 
 -- special games + defauly
 local gameid = game.GameId
@@ -15,9 +15,9 @@ local gameid = game.GameId
 if gameid == 9908641400 then
     loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/ad427b543b86b73fbc2a007ece7b358d.lua"))() -- ff3
 elseif gameid == 184199275 then
-    loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/c0b57bb9a7378ad6dc21e2f43587a18f.lua"))() -- football legends
-elseif gameid == 6505338302 then
     loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/ceb81d950c4857a7a1a04b8c84d80dd9.lua"))() -- uf
+elseif gameid == 6505338302 then
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/c0b57bb9a7378ad6dc21e2f43587a18f.lua"))() -- fbl
 else
     loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/2259faaddfcaa0d5acb74e84b2cd96b5.lua"))() -- defaulr
 end
