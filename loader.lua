bdc3c94774afffbd02919a191de02cc68254fdb2
azureblueday <gamerspace32@gmail.com>
2026-04-28 18:34:28 -0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,13 +1,9 @@
-local shit = game.GameId == 9908641400
 if game.GameId == 9908641400 then
     if getconnections then
         for _, v in next, getconnections(game:GetService("ScriptContext").Error) do
-            if v.Function then
-                v:Disable()
-            end
+            if v.Function then v:Disable() end
         end
     end
-
     if hookfunction then
         local Old; Old = hookfunction(game:GetService("LogService").GetLogHistory, function(...)
             local Results = Old(...)
@@ -20,19 +16,14 @@ if game.GameId == 9908641400 then
             end
             return Results
         end)
-
         local function findUpvalue(func, value)
             for _, upval in debug.getupvalues(func) do
-                if upval == value then
-                    return true
-                end
+                if upval == value then return true end
             end
             return false
         end
-
         local heartbeat = game:GetService("RunService").Heartbeat
         local dummy = Instance.new("BindableEvent")
-
         for _, func in getgc() do
             if typeof(func) == "function" and islclosure(func) then
                 if findUpvalue(func, heartbeat) then
@@ -48,26 +39,27 @@ if game.GameId == 9908641400 then
         end
     end
 end
-if (not hookfunction or not hookmetamethod) and not shit then
+
+if (identifyexecutor() == "Xeno" or identifyexecutor() == "Solara") and game.GameId ~= 9908641400 then
     game:GetService("Players").LocalPlayer:Kick('Nova | Unsupported Executor!')
     return
 end
 
 if not script_key then
-	game:GetService("Players").LocalPlayer:Kick("Nova | Please make sure you include the script_key part ABOVE the loadstring, otherwise Luarmor will not be able to recognize that you bought.")
-	return
+    game:GetService("Players").LocalPlayer:Kick("Nova | Please make sure you include the script_key part ABOVE the loadstring, otherwise Luarmor will not be able to recognize that you bought.")
+    return
 end
 
 if game.GameId == 9908641400 then
-     if hookfunction and hookmetamethod then
-	loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/ad427b543b86b73fbc2a007ece7b358d.lua"))()
-     else
-	loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/e8a512ea02aa7f12332fd74066499f05.lua"))()
-     end -- FF3
+    if identifyexecutor() ~= "Xeno" and identifyexecutor() ~= "Solara" then
+        loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/ad427b543b86b73fbc2a007ece7b358d.lua"))()
+    else
+        loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/e8a512ea02aa7f12332fd74066499f05.lua"))()
+    end
 elseif game.GameId == 6505338302 then
-     loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/c0b57bb9a7378ad6dc21e2f43587a18f.lua"))() -- Football Legends2
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/c0b57bb9a7378ad6dc21e2f43587a18f.lua"))() -- Football Legends
 elseif game.GameId == 184199275 then
-      loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/ceb81d950c4857a7a1a04b8c84d80dd9.lua"))() -- Universe Football
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/ceb81d950c4857a7a1a04b8c84d80dd9.lua"))() -- Universe Football
 else
-     loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/2259faaddfcaa0d5acb74e84b2cd96b5.lua"))() -- Main
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/2259faaddfcaa0d5acb74e84b2cd96b5.lua"))() -- Main
 end
