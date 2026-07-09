b683528910617e35e3e20ca7185f309890e0a750
azureblueday <gamerspace32@gmail.com>
2026-07-09 11:13:50 -0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -36,7 +36,7 @@ if game.GameId == 9908641400 then
 end
 
 if game.GameId == 9908641400 then
-    if identifyexecutor() ~= "Xeno" and identifyexecutor() ~= "Solara" and identifyexecutor() ~= "Seliware" and identifyexecutor() ~= "Velocity" then
+    if identifyexecutor() ~= "Xeno" and identifyexecutor() ~= "Solara" and identifyexecutor() ~= "Seliware" and identifyexecutor() ~= "Velocity" and identifyexecutor() ~= "Real" then
         loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/ad427b543b86b73fbc2a007ece7b358d.lua"))() -- Football Fusion 3
     else
         loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/ebf35a3f64beaa6be88055993bb074fc.lua"))() -- Football Fusion 3 (Alt Script)
