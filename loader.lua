cb28bedab2af036f9c1229bcf113dc63d727a226
azureblueday <gamerspace32@gmail.com>
2026-07-16 12:39:58 -0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,23 @@
-game:GetService("Players").LocalPlayer:Kick("Nova | Please get the new script at the script panel channel in the discord. (Auth service switched).")
+
+if (identifyexecutor() == "Xeno" or identifyexecutor() == "Solara") and game.GameId ~= 9908641400 then
+    game:GetService("Players").LocalPlayer:Kick('Nova | Unsupported Executor!')
+    return
+end
+
+if game.GameId == 9908641400 then
+    if identifyexecutor() ~= "Xeno" and identifyexecutor() ~= "Solara" and identifyexecutor() ~= "Seliware" and identifyexecutor() ~= "Velocity" and identifyexecutor() ~= "Real" then
+        loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/ad427b543b86b73fbc2a007ece7b358d.lua"))() -- Football Fusion 3
+    else
+        loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/ebf35a3f64beaa6be88055993bb074fc.lua"))() -- Football Fusion 3 (Alt Script)
+    end
+elseif game.GameId == 6505338302 then
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/c0b57bb9a7378ad6dc21e2f43587a18f.lua"))() -- Football Legends
+elseif game.GameId == 184199275 then
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/ceb81d950c4857a7a1a04b8c84d80dd9.lua"))() -- Universe Football
+elseif game.GameId == 5113572498 then
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/73482993c40862f621e0df5249a6260c.lua"))() -- College Football
+elseif game.GameId == 3032132418 then
+    game:GetService("Players").LocalPlayer:Kick('Nova | HCBB is Offline!') -- HCBB
+else
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/2259faaddfcaa0d5acb74e84b2cd96b5.lua"))() -- Main
+end
